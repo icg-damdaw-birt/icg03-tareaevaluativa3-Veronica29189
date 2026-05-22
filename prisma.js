0537ee6cd5158948019e4be85bd3e16dd5fd0283
Veronica29189 <verodriguez@birt.eus>
2026-05-22 09:31:34 +0200

    Add files via upload

---
+++ prisma.js
@@ -0,0 +1,11 @@
+// Configuración centralizada de Prisma para Prisma 7
+// Este archivo exporta una instancia única del cliente de Prisma
+const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { Pool } = require('pg');
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+module.exports = prisma;
